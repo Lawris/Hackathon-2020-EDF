6904609be349d9e03ddc678b72c6d30dfb1dbc59
Frau Axel <axel.frau.tr@gmail.com>
2020-12-17 22:27:11 +0100

    added top appbar

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,7 @@ import MainProps from './MainProps';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import Settings from '../Settings/Settings';
 import Conversation from '../Conversation/Conversation';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Button, Menu, MenuItem, AppBar, Toolbar, Typography, IconButton, MenuIcon } from '@material-ui/core';
 
 
 const Main: React.FunctionComponent<MainProps> = (props) => {
@@ -17,27 +17,39 @@ const Main: React.FunctionComponent<MainProps> = (props) => {
     setAnchorEl(null);
   };
     return (
-        <Router>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>Menu</Button>
-            <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}>
-                    <MenuItem onClick={handleClose}><Link to="/app/conversationlist">Liste de Conversation</Link></MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
-            <Link to="/app/settings">settings</Link><br/>
-            <Link to="/app/settings">settings</Link><br/>
-            <Link to="/app/settings">settings</Link><br/>
-            <Link to="/app/settings">settings</Link><br/>
-            <Switch>
-                <Route path="/app/conversation" component={Conversation} exact/>
-                <Route path="/app/settings" component={Settings} exact/>
-            </Switch>
-        </Router>
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className="menu-button" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className="title">
+                    News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Router>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}>
+                        <MenuItem onClick={handleClose}><Link to="/app/conversationlist">Liste de Conversation</Link></MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+                <Link to="/app/settings">settings</Link><br/>
+                <Link to="/app/settings">settings</Link><br/>
+                <Link to="/app/settings">settings</Link><br/>
+                <Link to="/app/settings">settings</Link><br/>
+                <Switch>
+                    <Route path="/app/conversation" component={Conversation} exact/>
+                    <Route path="/app/settings" component={Settings} exact/>
+                </Switch>
+            </Router>
+        </div>
     )
 }
 
