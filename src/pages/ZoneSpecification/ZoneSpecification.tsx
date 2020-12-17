63267dfa5ba150c33f1489fb382975b086b33d74
LisaFicello <figarelli.lisa@ntymail.com>
2020-12-18 00:05:40 +0100

    modif design specification zone

--- src/pages/ZoneSpecification/ZoneSpecification.tsx
+++ src/pages/ZoneSpecification/ZoneSpecification.tsx
@@ -1,5 +1,6 @@
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel } from '@material-ui/core';
 import React, {Component} from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 interface ZoneState {
     [key: string]: any;
@@ -9,6 +10,19 @@ interface ZoneProps {
     
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  }),
+);
+
+
+
 export default class Zone extends React.Component<ZoneProps, ZoneState>{
     constructor(props: ZoneProps){
         super(props);
@@ -16,53 +30,51 @@ export default class Zone extends React.Component<ZoneProps, ZoneState>{
             userMatricule: '',
             userPassword: '',
         }
+        const classes = useStyles();   
     }
     render(){
         return <div>
-          <FormControl component="fieldset">
+          <div className={classes.root}>
+          <FormControl component="fieldset" className={classes.formControl}>
             <FormLabel component="legend">Sélectionner la ou les zone(s) d'intervention</FormLabel>
               <FormGroup aria-label="position" row>
                 <FormControlLabel
                   value="BOGB"
                   control={<Checkbox color="primary" />}
                   label="BOGB"
-                  labelPlacement="end"
-                /><br/>
+                />
                 <FormControlLabel
                   value="BOCPO"
                   control={<Checkbox color="primary" />}
                   label="BOCPO"
-                  labelPlacement="end"
-                /><br/>
+                />
                 <FormControlLabel
                   value="BOBA"
                   control={<Checkbox color="primary" />}
                   label="BOBA"
-                  labelPlacement="end"
-                /><br/>
+                />
                 <FormControlLabel
                   value="BOES"
                   control={<Checkbox color="primary" />}
                   label="BOES"
-                  labelPlacement="end"
-                /><br/>
+                />
                 <FormControlLabel
                   value="BOVA"
                   control={<Checkbox color="primary" />}
                   label="BOVA"
-                  labelPlacement="end"
-                /><br/>
+                />
                 <FormControlLabel
                   value="BOGA"
                   control={<Checkbox color="primary" />}
                   label="BOGA"
-                  labelPlacement="end"
-                /><br/>
+                />
               </FormGroup>
-          </FormControl><br/>
+          </FormControl>
+          </div>  
+          <br/><br/>
           <Button variant="contained" color="primary" href="#contained-buttons">
             Confirmer
-          </Button>
+          </Button><br/><br/>
         </div>
     }
 
