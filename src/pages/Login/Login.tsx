c7b081351d74948e3dff3b383848545a05ce9e61
Frau Axel <axel.frau.tr@gmail.com>
2020-12-18 07:14:33 +0100

    Lot of front update

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import { Button, TextField } from '@material-ui/core';
 import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom';
 import LoginProps from '../../Model/LoginProps';
+import './Login.css'
 
 interface LoginState {
     [key: string]: any;
@@ -15,12 +16,23 @@ export default class Login extends React.Component<LoginProps, LoginState>{
             userPassword: '',
         }
     }
+
+    
+
     render(){
-        return <div>
-            <TextField id="outlined-basic" label="Matricule" variant="outlined" required/><br/>
-            <TextField id="outlined-basic" label="Password" variant="outlined" required/><br/><br/>
-            <Button variant="contained" color="primary" href="/app">Connexion</Button>
-        </div>
+        return (
+            <div>
+                <img className="img-login" src="https://storage.lebonguide.com/crop-1600x700/7/94/565CF6F3-EA3E-48DB-B255-668E5856CCEB.png" alt="background-image"/>
+                <div className="frame-login">
+                    <div className='title-login'><h2>CONNEXION</h2></div>
+                    <div className="login-form">
+                        <TextField className="input-login" id="outlined-basic" label="Matricule" variant="outlined" /><br/>
+                        <TextField className='input-login' id="outlined-basic" label="Password" variant="outlined" /><br/><br/>
+                        <Button className='button-login' variant="contained" color="primary" href="/app">Connexion</Button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
 }
--- src/pages/ZoneSpecification/ZoneSpecification.tsx
+++ src/pages/ZoneSpecification/ZoneSpecification.tsx
@@ -1,6 +1,7 @@
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel } from '@material-ui/core';
 import React, {Component} from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import imgcorse from '../../../public/assets/img-corse-map.png'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,44 +16,46 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Zone (){
   const classes = useStyles();
-  return( <div>
-          <div className={classes.root}>
-          <FormControl component="fieldset" className={classes.formControl}>
-            <FormLabel component="legend">Sélectionner la ou les zone(s) d'intervention</FormLabel>
-              <FormGroup>
-                <FormControlLabel
-                  value="BOGB"
-                  control={<Checkbox color="primary" />}
-                  label="BOGB - Grand Bastia"
-                />
-                <FormControlLabel
-                  value="BOCPO"
-                  control={<Checkbox color="primary" />}
-                  label="BOCPO - Corte/Ghisonaccia"
-                />
-                <FormControlLabel
-                  value="BOBA"
-                  control={<Checkbox color="primary" />}
-                  label="BOBA - Balagne"
-                />
-                <FormControlLabel
-                  value="BOES"
-                  control={<Checkbox color="primary" />}
-                  label="BOES - Extrême Sud"
-                />
-                <FormControlLabel
-                  value="BOVA"
-                  control={<Checkbox color="primary" />}
-                  label="BOVA - Valincu"
-                />
-                <FormControlLabel
-                  value="BOGA"
-                  control={<Checkbox color="primary" />}
-                  label="BOGA - Grand Ajaccio"
-                />
-              </FormGroup>
-          </FormControl>
-          </div>  
+  return( 
+        <div>
+            <img src={imgcorse} alt="iage map corse"/>
+            <div className={classes.root}>
+                <FormControl component="fieldset" className={classes.formControl}>
+                    <FormLabel component="legend">Sélectionner la ou les zone(s) d'intervention</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                        value="BOGB"
+                        control={<Checkbox color="primary" />}
+                        label="BOGB - Grand Bastia"
+                        />
+                        <FormControlLabel
+                        value="BOCPO"
+                        control={<Checkbox color="primary" />}
+                        label="BOCPO - Corte/Ghisonaccia"
+                        />
+                        <FormControlLabel
+                        value="BOBA"
+                        control={<Checkbox color="primary" />}
+                        label="BOBA - Balagne"
+                        />
+                        <FormControlLabel
+                        value="BOES"
+                        control={<Checkbox color="primary" />}
+                        label="BOES - Extrême Sud"
+                        />
+                        <FormControlLabel
+                        value="BOVA"
+                        control={<Checkbox color="primary" />}
+                        label="BOVA - Valincu"
+                        />
+                        <FormControlLabel
+                        value="BOGA"
+                        control={<Checkbox color="primary" />}
+                        label="BOGA - Grand Ajaccio"
+                        />
+                    </FormGroup>
+                </FormControl>
+            </div>
           <br/><br/>
           <Button variant="contained" color="primary" href="#contained-buttons">
             Confirmer
